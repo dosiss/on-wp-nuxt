5a07fb3f79d1df841225724341286e49ccf53e43
Alexander Kharin <alexander.kharin@gmail.com>
2024-08-20 19:38:41 +0300

    tg script

--- pages/cart.vue
+++ pages/cart.vue
@@ -2,12 +2,18 @@
 import { useRoute } from 'vue-router';
 import Cart from '~/components/Cart.vue';
 import { useCartStore } from '../store/cart';
+import { MainButton, useWebAppPopup, useWebAppTheme } from 'vue-tg'
 
 const route = useRoute();
+const { showAlert } = useWebAppPopup()
+//const { setBackgroundColor } = useWebAppTheme()
 </script>
 <template>
   <div class="bg-grey-100 min-h-screen">
     <TheHeader></TheHeader>
     <Cart></Cart>
+    <ClientOnly>
+        <MainButton text="Заказать" @click="() => showAlert('Спасибо за ваш заказ! Мы свяжемся с вами в Телеграм.')" />
+    </ClientOnly>
   </div>
 </template>
