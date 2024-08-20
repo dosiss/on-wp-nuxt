9655568f03b4529b653e0436cfa04dcc35a323a7
Alexander Kharin <alexander.kharin@gmail.com>
2024-08-20 13:25:46 +0300

    pinia force added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,9 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  alias: {
-    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
-  },
+  // alias: {
+  //   pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
+  // },
   app: {
     head: {
       script: [{ src: 'https://telegram.org/js/telegram-web-app.js' }],
