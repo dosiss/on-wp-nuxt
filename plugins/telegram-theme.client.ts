1fdd5bb79409c23ad2a059c17d8fb19c8c538ea2
Alexander Kharin <alexander.kharin@gmail.com>
2024-08-21 10:22:46 +0300

    bg color change

---
+++ plugins/telegram-theme.client.ts
@@ -0,0 +1,9 @@
+import { defineNuxtPlugin } from '#app';
+import { useWebAppTheme } from 'vue-tg';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const { setBackgroundColor } = useWebAppTheme();
+
+  // Set a default background color for the entire Telegram Mini App
+  setBackgroundColor('#ffffff'); // Replace with your desired color
+});
