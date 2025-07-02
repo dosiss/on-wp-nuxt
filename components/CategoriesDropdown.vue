7b7b2930df17d2f982772948dde350280ca21707
Alexander Kharin <alexander.kharin@gmail.com>
2025-07-02 20:10:05 +0300

    brands drpdwn add

--- components/CategoriesDropdown.vue
+++ components/CategoriesDropdown.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="relative flex-grow mr-8" :style="{ display: isVisible ? 'block' : 'none' }">
+  <div class="relative flex-grow mr-6 min-w-[200px]" :style="{ display: isVisible ? 'block' : 'none' }">
     <button
       @click="toggleDropdown"
       class="bg-gray-200 text-gray-700 px-4 py-2 rounded-lg w-full text-left flex items-center justify-between"
     >
       Категории
       <svg
-        class="w-5 h-5"
+        class="w-5 h-5 ml-2 flex-shrink-0"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -15,7 +15,7 @@
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
       </svg>
     </button>
-    <div v-if="dropdownOpen" class="absolute mt-1 w-full bg-white shadow-lg rounded-lg z-10">
+    <div v-if="dropdownOpen" class="fixed mt-1 bg-white shadow-lg rounded-lg z-50" style="width: 200px;">
       <ul>
         <li v-for="parent in parentCategories" :key="parent.slug" class="p-2">
           <div @click="toggleSubmenu(parent.slug)" class="flex justify-between cursor-pointer">
